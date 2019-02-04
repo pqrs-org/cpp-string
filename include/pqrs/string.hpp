9b8a87b9bbea98ab6ca1b64e69cb5d55ea41c3b0
Takayama Fumihiko <tekezo@pqrs.org>
2019-02-05 00:47:09 +0900

    add starts_with,ends_with

--- include/pqrs/string.hpp
+++ include/pqrs/string.hpp
@@ -6,4 +6,5 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See http://www.boost.org/LICENSE_1_0.txt)
 
+#include "string/predicate.hpp"
 #include "string/trim.hpp"
---
+++ tests/src/test_predicate.cpp
@@ -0,0 +1,41 @@
+#include <catch2/catch.hpp>
+
+#include <pqrs/string.hpp>
+
+TEST_CASE("starts_with") {
+  {
+    std::string s("1234");
+    REQUIRE(pqrs::string::starts_with(s, ""));
+    REQUIRE(pqrs::string::starts_with(s, "1"));
+    REQUIRE(pqrs::string::starts_with(s, "12"));
+    REQUIRE(pqrs::string::starts_with(s, "123"));
+    REQUIRE(pqrs::string::starts_with(s, "1234"));
+    REQUIRE(!pqrs::string::starts_with(s, "12345"));
+    REQUIRE(!pqrs::string::starts_with(s, "2"));
+    REQUIRE(!pqrs::string::starts_with(s, "23456"));
+  }
+  {
+    std::string s("");
+    REQUIRE(pqrs::string::starts_with(s, ""));
+    REQUIRE(!pqrs::string::starts_with(s, "1"));
+  }
+}
+
+TEST_CASE("ends_with") {
+  {
+    std::string s("1234");
+    REQUIRE(pqrs::string::ends_with(s, ""));
+    REQUIRE(pqrs::string::ends_with(s, "4"));
+    REQUIRE(pqrs::string::ends_with(s, "34"));
+    REQUIRE(pqrs::string::ends_with(s, "234"));
+    REQUIRE(pqrs::string::ends_with(s, "1234"));
+    REQUIRE(!pqrs::string::ends_with(s, "01234"));
+    REQUIRE(!pqrs::string::ends_with(s, "3"));
+    REQUIRE(!pqrs::string::ends_with(s, "00123"));
+  }
+  {
+    std::string s("");
+    REQUIRE(pqrs::string::ends_with(s, ""));
+    REQUIRE(!pqrs::string::ends_with(s, "1"));
+  }
+}
